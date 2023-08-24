f7cab44a1e9b7c09abe5e761c731fc5fb09c502c
Ernest <u202020229@upc.edu.pe>
2023-08-24 01:28:43 -0500

    AddServices

---
+++ Nimbus/Model/NimbusService.cs
@@ -0,0 +1,9 @@
+namespace Nimbus.Model;
+
+public class NimbusService
+{
+    public string Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public string Estado { get; set; }
+}
